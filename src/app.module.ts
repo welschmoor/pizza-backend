a8d414ef0c90bf8c966b832f264b30dea86a25d9
Johannes Welsch <welschmoor@gmail.com>
2022-04-26 16:20:55 +0200

    pizzaNest 1st commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+    }),
+  ],
+})
+export class AppModule {}
