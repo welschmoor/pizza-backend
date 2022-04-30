7075e662975f63da402f84c6402fb021a099962e
Johannes Welsch <welschmoor@gmail.com>
2022-04-30 23:42:44 +0200

    put envars into .env, added joi validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,23 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().valid().required(),
+        DB_PORT: Joi.string().valid().required(),
+        DB_USERNAME: Joi.string().valid().required(),
+        DB_PASSWORD: Joi.string().valid().required(),
+        DB_DATBASE: Joi.string().valid().required(),
+      }),
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/schema.gql',
@@ -14,17 +28,13 @@ import { ConfigModule } from '@nestjs/config';
     RestaurantsModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'ment',
-      password: '12345',
-      database: 'pizza',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATBASE,
       synchronize: true, //migrate on connect
       logging: true,
-    }),
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env',
     }),
   ],
 })
